76790770873b6eb9dbcef0fcc0d8d9670462db35
boinaohemam <boinaohemam@gmail.com>
2024-11-14 22:36:26 +0530

    dt.14.11.24

--- C/test.c
+++ C/test.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
-#include <stdio.h>
-#include <string.h>
 
-struct Book { 
-    int id;
-    char title[100];
-    int available;
-    struct Book* next;
-};
+int main() {
+    int n, i;
+    
+    printf("Enter array size: ");
+    scanf("%d", &n);
 
-struct Book* bookhead = NULL;
+    // Now that we know the size, declare the array
+    int arr[n];
 
-void addbook(int id, char title[], int available){
-   struct Book* newBook = (struct Book*)malloc(sizeof(struct Book));
-   newBook->id = id;
-   strcpy(newBook->title, title);
-   newBook->available = available;
-   newBook->next = bookhead;
-   bookhead = newBook;
-   printf("Book added successfully\n"); 
+    printf("Enter elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array elements are:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
 
