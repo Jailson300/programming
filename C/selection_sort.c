8b72a852f83eaba7a9f7f531017cd28427c95be5
boinaohemam <boinaohemam@gmail.com>
2024-11-14 22:19:23 +0530

    dt14.11.24

---
+++ C/selection_sort.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int n,i,j,temp=0,arr[10]={39,9,81,15,45,90,27,72,18};
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(arr[i]<arr[j]){
+                temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
