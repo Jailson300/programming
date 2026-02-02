943d73a53aa312ba2f81ffc52562474104b3cbed
boinaohemam <boinaohemam@gmail.com>
2026-02-02 22:42:22 +0530

    1

--- C/selection_sort.c
+++ C/selection_sort.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
 int main(){
-    int n,i,j,temp=0,arr[10]={39,9,81,15,45,90,27,72,18};
+    int n=5,i,j,temp=0,arr[10]={39,9,81,15,45,90,27,72,18};
     for(i=0;i<n;i++){
         for(j=i+1;j<n;j++){
-            if(arr[i]<arr[j]){
+            if(arr[i]>arr[j]){
                 temp = arr[i];
                 arr[i]=arr[j];
                 arr[j]=temp;
